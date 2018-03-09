8925282c49175278f2c9b7463336d4694f04a6be
emi kojima <emi8emi@gmail.com>
2018-03-09 10:52:36 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,9 +18,6 @@ class Doctor
   end 
   
   def patients
-    #* The `Doctor` class needs an instance method, `#patients`, that iterates over that doctor's appointments and collects the patient that belongs to each appointment. 
-
-   # binding.pry
     @appointments.collect {|appointment| appointment.patient}
   end
 end 
